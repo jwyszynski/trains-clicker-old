b8fce185dbe14ee2b9cbbed1f9a23e8196d63022
Jakub Wyszyński <33234553+jwyszynski@users.noreply.github.com>
2021-04-15 02:47:48 +0200

    Changes

--- index.js
+++ index.js
@@ -2,37 +2,40 @@ function TheTrain() {
   return (
     <div className="train">
       <TrainItem name="Loco 1" type="engine" />
-      <TrainItem name="Wagon 1" type="wagon" />
       <div className="clear"></div>
     </div>
   );
 }
 
 function TrainItem({ name, type }) {
-  const imgUrl = `https://www.twojehobby.eu/images/Wagon%20towarowy%20kryty%20typ%20G02%20Piko%2058945%20www.twojehobby%20(11).jpg`;
-  let styles = {
-    margin: "20px",
-    width: "200px",
-    height: "100px",
-    backgroundColor: "yellow",
-  };
+  const imgUrl = `/img/trains-profile.jpg`;
+
   return (
-    <div className="item">
-      <img src={imgUrl} className="ui mini rounded image" style={styles} />
-      <div className="content">
-        <h1 className="header">{name}</h1>
-        <div className="description">{type}</div>
-      </div>
+    <div className="floatRight">
+      <div className="content trainImage"></div>
     </div>
   );
 }
 
 function TrainView() {
   return (
-    <div className="container-md">
-      <main className="ui main text container">
+    <div className="">
+      <main className="">
         <TheTrain />
       </main>
+    </div>
+  );
+}
+
+function MidView() {
+  return (
+    <div className="row align-items-start">
+      <div className="col button-section">
+        <button className="btn btn-primary">Kliknij tutaj</button>
+      </div>
+      <div className="col stats">Stats -----------------------</div>
+      <div className="w-20 d-block d-md-none "></div>
+      <div className="col upgrades">Upgrades -----------------------------</div>
     </div>
   );
 }
@@ -41,13 +44,14 @@ function App() {
   return (
     <div className="container bg-light main">
       <TrainView />
+      <MidView />
       <AppFooter />
     </div>
   );
 }
 
 function AppFooter() {
-  return <h8> Wyszyński.go All rights reserved, but not sure. </h8>;
+  return <p> Wyszyński.go All rights reserved, but not sure. </p>;
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
